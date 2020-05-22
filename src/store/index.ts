f6589816d54e2b29614c0a1753edcbc1f30c173d
GaoCarri <gaocarri@163.com>
2020-05-22 22:15:57 +0800

    建立store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import auth from "@/api/auth"
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // auth
+    user: null,
+    isLogin: false,
   },
   mutations: {
+    // auth
+    setUser(state, payload) {
+      state.user = payload.user
+    },
+
+    setLogin(state, payload) {
+      state.isLogin = payload.isLogin
+    }
   },
   actions: {
+    // auth
+    login({ commit }, { username, password }) {
+      return auth.login({ username, password }).then((res: any) => {
+        commit('setUser', { user: res.data })
+        commit('setLogin', { isLogin: true })
+      })
+    },
+    async register({ commit }, { username, password }) {
+      let res: any = await auth.register({ username, password })
+      commit('setUser', { user: res.data })
+      commit('setLogin', { isLogin: true })
+      return res.data
+    }
   },
   modules: {
   }
